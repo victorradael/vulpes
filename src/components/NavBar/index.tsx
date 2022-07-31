b2391e97d6da1f4b6c1c59d37ac24151592db3bb
Victor Radael <victorradael@hotmail.com>
2022-07-31 17:29:12 -0300

    description

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -17,17 +17,17 @@ function NavigationBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="#link">Prices</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="/about">Sobre nós</Nav.Link>
+            <Nav.Link href="#link">Preços</Nav.Link>
+            <NavDropdown title="Outros" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Missão</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Visão
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Valores</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Fale conosco
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import ContentContainer from "../../components/ContentContainer";
 import CustomHead from "../../components/CustomHead";
 import { Footer } from "../../components/Footer";
+import { InfoCard } from "../../components/InfoCard";
 import Header from "../../views/Header";
 
 const LandingPage: NextPage = () => {
@@ -10,7 +11,7 @@ const LandingPage: NextPage = () => {
       <CustomHead />
       <ContentContainer>
         <Header />
-        <div>sdjifhsdnifcuj</div>
+        <InfoCard/>
         <Footer />
       </ContentContainer>
     </>
