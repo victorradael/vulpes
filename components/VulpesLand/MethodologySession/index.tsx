cefe5be25f28dd3512eea37c91a57d45f33bbe40
Celso Brito <celsofilho_ap@hotmail.com>
2023-05-31 09:39:24 -0300

    :sparkles: Add the Methodology Session
    1. Add the Methodology site Session;
    2. Add the Methodology site View;

---
+++ components/VulpesLand/MethodologySession/index.tsx
@@ -0,0 +1,12 @@
+import './styles.module.css';
+import styles from './styles.module.css';
+
+const MethodologySession = () => {
+  return (
+    <div className={styles.container} id='header'>
+      
+    </div>
+  );
+};
+
+export default MethodologySession;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,9 @@ import type { NextPage } from 'next';
 import ContentContainer from '../views/Container';
 import HeroView from '../views/VulpesLand/HeroView';
 import NavView from '../views/VulpesLand/NavView';
-import Link from 'next/link';
+import MethodologyView from '../views/VulpesLand/MethodologyView';
 import DataValuesView from '../views/VulpesLand/DataValuesView';
 import img_wpp_group from '../public/group-wpp.png';
-
-import Header from "../views/Thumbnail";
-import FooterView from "../views/FooterView";
-
 
 const LandingPage: NextPage = () => {
     return (
@@ -16,7 +12,7 @@ const LandingPage: NextPage = () => {
             <NavView></NavView>
             <HeroView></HeroView>
             <DataValuesView></DataValuesView>
-            </Head>
+            <MethodologyView></MethodologyView>
             <Header modalChangers={modalChanger}/>
         </ContentContainer>
     );
