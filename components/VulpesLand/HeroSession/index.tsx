80646555e480427b73d24b8f94f22a8c380aba0b
Celso Brito <celsofilho_ap@hotmail.com>
2023-08-01 10:54:05 -0300

    :sparkles: Add language animation on hero session
    1. Add animation of typing texts in hero session.

--- components/VulpesLand/HeroSession/index.tsx
+++ components/VulpesLand/HeroSession/index.tsx
@@ -2,13 +2,34 @@ import './styles.module.css';
 import styles from './styles.module.css';
 import Image from 'next/image';
 import imgHandArrow from '../../../public/LANDPAGE/hand-arrow-down.png';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 const HeroSession = () => {
+  const [ lang ] = useTypewriter({
+    words: ['Inglês', 'Espanhol', 'Alemão', 'Francês'],
+    loop: 0,
+    typeSpeed: 120,
+    deleteSpeed: 80,
+    delaySpeed: 2000,
+  });
+
+  const [ ola ] = useTypewriter({
+    words: ['Hello_', 'Hola_', 'Hallo_', 'Bonjour_'],
+    loop: 0,
+    typeSpeed: 0,
+    deleteSpeed: 0,
+    delaySpeed: 2000,
+  });
+
   return (
     <div className={styles.container} id='header'>
       <div className={styles.intro}>
-        <span className={styles.gray}>Hello_</span>
-        <h1 className={styles.title}>Aprenda <span className={styles.languages}>Inglês</span></h1>
+        <span className={styles.gray}>{ola}</span>
+        <h1 className={styles.title}>Aprenda <span className={styles.languages}>{lang}</span>
+        <span className={styles.cursor}>
+          <Cursor/>
+        </span>
+        </h1>
         <p className={styles.paragr}>de forma personalizada e com foco no seu objetivo</p>
         {/* <div className={styles.know__more__session}>
           <span className={styles.know__more}>Conheça a Vulpes</span>
