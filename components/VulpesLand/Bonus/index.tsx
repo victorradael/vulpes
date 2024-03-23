b303653e90a6cebd42dc4f2947016d16d68edb52
Celso Brito <celsofilho_ap@hotmail.com>
2024-03-23 11:51:00 -0300

    :sparkles: Add New Session FeedBack
    1. Remove Spanish;
    2. Update Intagram post pictures;
    3. Add New Session about FeedBack;

---
+++ components/VulpesLand/Bonus/index.tsx
@@ -0,0 +1,176 @@
+import styles from './styles.module.css';
+import { FaUser } from "react-icons/fa";
+
+const FeedBackSession = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>Nossos FeedBacks:</div>
+            <div className={styles.cards__group}>
+                <div className={styles.card} data-aos="fade-left" data-aos-offset={300} data-aos-duration={1000}>
+                    <span className={styles.people__brief}>
+                    &quot;
+                    Foi incrível, bastante dinâmica assim como a primeira. Conversar força a gente a entender melhor kkkk
+                    &quot;
+                    </span>
+                    <div className={styles.user__card}>
+                        <FaUser className={styles.user} />
+                        <div className={styles.card__texts}>
+                            <span className={styles.people__name}>Letícia Oliveira</span>
+                            <span className={styles.people__office}>Student</span>
+                        </div>
+                    </div>
+                    <div className={styles.break__line}></div>
+                </div>
+
+                <div className={styles.card} data-aos="fade-left" data-aos-offset={200} data-aos-duration={1000}>
+                    <span className={styles.people__brief}>
+                    &quot;
+                    Eu sinto que estou progredindo mais rápido
+                    &quot;
+                    <br/>
+                    <br/>
+                    &quot;
+                    No início eu fiquei muito nervoso, e aí eu falei que conseguia, e pedi para que ela me fizesse a pergunta que quisesse. E fiquei conversando com ela por 20 min.
+                    &quot;
+                    </span>
+                    <div className={styles.user__card}>
+                        <FaUser className={styles.user}/>
+                        <div className={styles.card__texts}>
+                            <span className={styles.people__name}>Claudionor</span>
+                            <span className={styles.people__office}>Student</span>
+                        </div>
+                    </div>
+                    <div className={styles.break__line}></div>
+                </div>
+
+                <div className={styles.card} data-aos="fade-left" data-aos-offset={200} data-aos-duration={1000}>
+                    <span className={styles.people__brief}>
+                    &quot;
+                    A Talita é ótima
+                    &quot;
+                    <br/>
+                    <br/>
+                    &quot;
+                    Não só a Talita, mas até mesmo quando precisamos remarcar as aulas e cai com outros professores, eles são ótimos também!
+                    &quot;
+                    </span>
+                    <div className={styles.user__card}>
+                        <FaUser className={styles.user}/>
+                        <div className={styles.card__texts}>
+                            <span className={styles.people__name}>Douglas Freire</span>
+                            <span className={styles.people__office}>Student</span>
+                        </div>
+                    </div>
+                    <div className={styles.break__line}></div>
+                </div>
+
+                <div className={styles.card} data-aos="fade-left" data-aos-offset={200} data-aos-duration={1000}>
+                    <span className={styles.people__brief}>
+                    &quot;
+                    Cara, me desenvolvi muito esse ano, principalmente pela atuação da Sarah. Participei do Business class, curti muito... agora estou curtindo bastante a didática e metodologia do Gabriel, meu chará
+                    &quot;
+                    <br/>
+                    <br/>
+                    &quot;
+                    No mais, só agradecer toda a equipe e administrativo pela dedicação e atendimento, sempre excelente!
+                    &quot;
+                    <br/>
+                    <br/>
+                    &quot;
+                    ...mas eu gosto muito, falo de todo mundo e indico a Vulpes para os meus colegas.
+                    &quot;
+                    </span>
+                    <div className={styles.user__card}>
+                        <FaUser className={styles.user}/>
+                        <div className={styles.card__texts}>
+                            <span className={styles.people__name}>Gabriel</span>
+                            <span className={styles.people__office}>Student</span>
+                        </div>
+                    </div>
+                    <div className={styles.break__line}></div>
+                </div>
+
+                <div className={styles.card} data-aos="fade-left" data-aos-offset={200} data-aos-duration={1000}>
+                    <span className={styles.people__brief}>
+                    &quot;
+                    Desejo expressar meus mais sinceros agradecimentos à instituição Vulpes pela recepção calorosa desde a primeira correspondência enviada. Em todas as ocasiões em que necessitei de reposição de aulas, fui prontamente atendido, sendo evidente a qualidade do ensino proporcionado pela Vulpes. Sua capacidade de adaptação às necessidades dos alunos é altamente benéfica para a facilitação da aprendizagem. Não encontro motivos para qualquer tipo de insatisfação com a Vulpes, apenas para parabenizar efusivamente a instituição pela excelência no ensino e pelo zelo dedicado aos alunos. Gostraria de destacar, em especial, o professor Nicolas por sua didática exemplar.
+                    &quot;
+                    </span>
+                    <div className={styles.user__card}>
+                        <FaUser className={styles.user}/>
+                        <div className={styles.card__texts}>
+                            <span className={styles.people__name}>Matheus Carvalho</span>
+                            <span className={styles.people__office}>Student</span>
+                        </div>
+                    </div>
+                    <div className={styles.break__line}></div>
+                </div>
+
+                <div className={styles.card} data-aos="fade-left" data-aos-offset={200} data-aos-duration={1000}>
+                    <span className={styles.people__brief}>
+                        &quot;
+                        A Vulpes é uma escola diferenciada. Ela se molda ao que o aluno precisa, trazendo conteúdo específico, de acordo com o que se almeja com o idioma. Um ponto de destaque é a relação entre professor e aluno. Desde que iniciei na escola tive a oportunidade de ter aula com vários professores com sotaques, didáticas e perfis diferentes, que sempre se dedicaram para trazer aulas interessantes e pertinentes ao meu objetivo, o que sem dúvidas ajudou na minha melhor compreensão do Inglês, mas também tiveram a sensibilidade e empatia em entender que nem sempre o que havia sido programado para determinada aula seria a melhor opção para o momento, já que as vezes em uma boa conversa dos acontecimentos da rotina pessoal e profissional já se consegue extrair um aprendizado natural, o que deixa tudo mais leve e menos formal, ajudando a criar laços que vão muito além de uma chamada de vídeo semanal.
+                        &quot;
+                    </span>
+                    <div className={styles.user__card}>
+                        <FaUser className={styles.user}/>
+                        <div className={styles.card__texts}>
+                            <span className={styles.people__name}>Otavio Nery</span>
+                            <span className={styles.people__office}>Student</span>
+                        </div>
+                    </div>
+                    <div className={styles.break__line}></div>
+                </div>
+
+                <div className={styles.card} data-aos="fade-left" data-aos-offset={200} data-aos-duration={1000}>
+                    <span className={styles.people__brief}>
+                        &quot;
+                        As aulas são bem produtivas
+                        &quot;
+                        <br/>
+                        <br/>
+                        &quot;
+                        tanto a em dupla quanto a individual
+                        &quot;
+                        <br/>
+                        <br/>
+                        &quot;
+                        percebi que estou avançando mais em relação a conversação
+                        &quot;
+                    </span>
+                    <div className={styles.user__card}>
+                        <FaUser className={styles.user}/>
+                        <div className={styles.card__texts}>
+                            <span className={styles.people__name}>Cecília Martins</span>
+                            <span className={styles.people__office}>Student</span>
+                        </div>
+                    </div>
+                    <div className={styles.break__line}></div>
+                </div>
+
+                <div className={styles.card} data-aos="fade-left" data-aos-offset={200} data-aos-duration={1000}>
+                    <span className={styles.people__brief}>
+                        &quot;
+                        Tudo tranquilo. Se melhorar, estraga!😅 
+                        &quot;
+                        <br/>
+                        <br/>
+                        &quot;
+                        Meu teacher é o Murilo. As aulas estão ótimas. Ele é pontual, eficiente, prepara as aulas com temas que me interessam e traz sempre a gramática contextualizada. Tenho homework, que eu acho muito importante. Está tudo de acordo. Acho que não tenho nada a questionar.
+                        &quot;
+                    </span>
+                    <div className={styles.user__card}>
+                        <FaUser className={styles.user}/>
+                        <div className={styles.card__texts}>
+                            <span className={styles.people__name}>Silvana Pereira</span>
+                            <span className={styles.people__office}>Student</span>
+                        </div>
+                    </div>
+                    <div className={styles.break__line}></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeedBackSession;
---
+++ views/VulpesLand/BonusView/index.tsx
@@ -0,0 +1,14 @@
+import './styles.module.css';
+import styles from './styles.module.css';
+import FeedBackSession from '../../../components/VulpesLand/Bonus';
+import { TbDiscountCheckFilled } from "react-icons/tb";
+
+const FeedBackView = () => {
+  return (
+    <div className={styles.container}>
+      <FeedBackSession />
+    </div >
+  );
+};
+
+export default FeedBackView;
