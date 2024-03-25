e62b7a41fb034aae32e4b2e05e318be7be043ee5
Celso Brito <celsofilho_ap@hotmail.com>
2024-03-25 11:01:22 -0300

    :bug: Handled the agent contact

--- public/Assets/Contact-Agent.ts
+++ public/Assets/Contact-Agent.ts
@@ -1,4 +1,4 @@
-export const contactAgent = 'https://wa.me/553182310061';
+export const contactAgent = 'https://wa.me/5527989012676';
 export const jouneyContact = 'https://api.whatsapp.com/send/?phone=5527989012676&text=Ol%C3%A1%21+Gostaria+de+saber+mais+sobre+as+aulas+ingl%C3%AAs+da+Vulpes&type=phone_number&app_absent=0';
 export const overseasContact = 'https://api.whatsapp.com/send/?phone=5527989012676&text=Ol%C3%A1%21+Gostaria+de+saber+mais+informa%C3%A7%C3%B5es+sobre+o+Overseas&type=phone_number&app_absent=0';
 export const proAceContact = 'https://api.whatsapp.com/send/?phone=5527989012676&text=Ol%C3%A1%21+Gostaria+de+saber+mais+informa%C3%A7%C3%B5es+sobre+o+Pro+ace&type=phone_number&app_absent=0';
